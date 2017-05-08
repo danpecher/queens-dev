1b46f9692deec43558df3a61e2e1e8daedfe281e
Dan Pecher <dan.pecher@gmail.com>
2017-05-08 15:29:09 -0500

    require invoice number

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,5 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :number
   has_many :invoice_items, dependent: :destroy
   accepts_nested_attributes_for :invoice_items, allow_destroy: true
 end
